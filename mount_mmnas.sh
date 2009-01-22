833970e16a42f974454466f51d27eaa570a78a90
gmacario <gmacario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-01-22 20:45:37 +0000

    Changed project name to LUPIN, added copyright
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@7469 23512697-871b-0410-bc7e-d018c10ccd9e

--- mount_mmnas.sh
+++ mount_mmnas.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # =============================================================================
-# Project:	NBT
+# Project:	LUPIN
 #
 # Purpose:	Mount partition fron Magneti Marelli NAS
 #
@@ -9,17 +9,17 @@
 #
 # Note:		Should configure username,etc.
 #
-# Copyright 2007,2008 Magneti Marelli Electronic Systems - All Rights Reserved
+# Copyright 2007-2009 Magneti Marelli Electronic Systems - All Rights Reserved
 # =============================================================================
 
-SMBSHARE=//itven1nnas1.venaria.marelli.it/BMW_NBT
-MOUNTPOINT=/mnt/bmw_nbt
+SMBSHARE=//itven1nnas1.venaria.marelli.it/LUPIN
+MOUNTPOINT=/mnt/lupin
 MOUNT_OPTS=uid=${USER}
 MOUNT_OPTS+=,username=macario
 MOUNT_OPTS+=,workgroup=MMEMEA
 #MOUNT_OPTS+=,password=VERYSECRET
 
-set -x
+#set -x
 
 sudo mkdir -p ${MOUNTPOINT}
 sudo smbmount ${SMBSHARE} ${MOUNTPOINT} -o ${MOUNT_OPTS} && \
