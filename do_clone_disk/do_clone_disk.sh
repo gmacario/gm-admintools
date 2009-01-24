699522da3afe081f1e12a67f229e050a18bb810f
gmacario <gmacario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-01-24 06:11:13 +0000

    More sanity checks on disk geometry compatibility
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@7514 23512697-871b-0410-bc7e-d018c10ccd9e

--- do_clone_disk/do_clone_disk.sh
+++ do_clone_disk/do_clone_disk.sh
@@ -71,6 +71,52 @@ if [ `mount | grep ${DEV_DEST} | wc -l` -gt 0 ]; then
     exit 1
 fi
 
+# Make sure that source and dest have the same blocksize
+blksize_source=`LANG=C fdisk -l ${DEV_SOURCE} | grep "^Units"`
+blksize_dest=`LANG=C fdisk -l ${DEV_DEST} | grep "^Units"`
+#echo "DBG: DEV_SOURCE ${blksize_source}"
+#echo "DBG: DEV_DEST   ${blksize_dest}"
+if [ "${blksize_source}" != "${blksize_dest}" ]; then
+	echo "ERROR: source and dest block sizes do not match"
+	exit 1
+fi
+
+# Verify that ${DEV_SOURCE} and ${DEV_DEST} disk geometries are compatible
+geom_source=`LANG=C fdisk -l ${DEV_SOURCE} | grep "cylinders$"`
+geom_dest=`LANG=C fdisk -l ${DEV_DEST} | grep "cylinders$"`
+#echo "DBG: DEV_SOURCE ${geom_source}"
+#echo "DBG: DEV_DEST   ${geom_dest}"
+if [ "${geom_source}" != "${geom_dest}" ]; then
+	echo "WARNING: source and dest geometries do not match"
+	echo "  ${DEV_SOURCE}: ${geom_source}"
+	echo "  ${DEV_DEST}: ${geom_dest}"
+	echo
+fi
+heads_source=`echo ${geom_source} | awk '// {print $1}'`
+heads_dest=`echo ${geom_dest} | awk '// {print $1}'`
+#echo "DBG: DEV_SOURCE heads: ${heads_source}"
+#echo "DBG: DEV_DEST   heads: ${heads_dest}"
+if [ "${heads_source}" != "${heads_dest}" ]; then
+	echo "ERROR: incompatible heads: source:${heads_source} and dest:${heads_dest}"
+	exit 1
+fi
+sectrk_source=`echo ${geom_source} | awk '// {print $3}'`
+sectrk_dest=`echo ${geom_dest} | awk '// {print $3}'`
+#echo "DBG: DEV_SOURCE sectors/track: ${sectrk_source}"
+#echo "DBG: DEV_DEST   sectors/track: ${sectrk_dest}"
+if [ "${sectrk_source}" != "${sectrk_dest}" ]; then
+	echo "ERROR: incompatible sectors/track: source (${sectrk_source}) dest:${sectrk_dest}"
+	exit 1
+fi
+cylinders_source=`echo ${geom_source} | awk '// {print $5}'`
+cylinders_dest=`echo ${geom_dest} | awk '// {print $5}'`
+#echo "DBG: DEV_SOURCE cylinders: ${cylinders_source}"
+#echo "DBG: DEV_DEST   cylinders: ${cylinders_dest}"
+if [ "${cylinders_source}" -gt "${cylinders_dest}" ]; then
+	echo "ERROR: incompatible cylinders: source:${cylinders_source} dest:${cylinders_dest}"
+	exit 1
+fi
+
 # TODO: Make sure DEV_DEST is empty
 
 echo "WARNING: this procedure will destroy contents on ${DEV_DEST}"
@@ -83,6 +129,7 @@ fi
 
 # Sanity checks OK, go ahead...
 echo Cloning disk from ${DEV_SOURCE} to ${DEV_DEST}, please wait...
+
 
 # TODO: Verify partition layout on ${DEV_SOURCE}
 
