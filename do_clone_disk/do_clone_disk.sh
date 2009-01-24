cc44bac60b22e576a42da5901f149f09c1c75929
gmacario <gmacario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-01-24 09:46:55 +0000

    Delete existing partitions on DEV_DEST
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@7519 23512697-871b-0410-bc7e-d018c10ccd9e

--- do_clone_disk/do_clone_disk.sh
+++ do_clone_disk/do_clone_disk.sh
@@ -16,19 +16,25 @@
 DEV_SOURCE=/dev/sda
 #
 # Destination device (WARNING: WILL DESTROY CONTENTS!!!)
-DEV_DEST=/dev/sdb
+DEV_DEST=/dev/sdc
 
 
 # Advanced options - USE AT YOUR OWN RISK!!!
 #
-# Do not make consistency checks on DEV_SOURCE vs. DEV_DEST disk geometry
+# Do no consistency checks on DEV_SOURCE vs. DEV_DEST disk geometry
 OPT_NO_GEOMETRY_CHECK=true
 #
+# Create Master Boot Record on DEV_DEST
 #OPT_CREATE_DEST_MBR=true
 #
-#OPT_CREATE_DEST_PARTITIONS=true
+# Create partitions on DEV_DEST (same layout as DEV_SOURCE)
+OPT_CREATE_DEST_PARTITIONS=true
 #
-#OPT_ADJUST_LAST_PARTITION=true
+# Format partitions on DEV_DEST (implicit if OPT_CREATE_DEST_PARTITIONS)
+#OPT_FORMAT_DEST_PARTITIONS=true
+#
+# Specify the number of the partition to resize in case the two disks have different capacity
+#OPT_RESIZE_PARTITION=2
 
 # End of configurable parameters
 # -----------------------------------------------------------------------------
@@ -54,7 +60,11 @@ fi
 #/^Disk \/dev/	{ print $0 }
 #'
 
-echo "*** This is the SOURCE disk ***"
+echo "*** List of available disks on the system:"
+LANG=C fdisk -l | grep "^Disk /"
+echo
+
+echo "*** SOURCE disk information (${DEV_SOURCE}) ***"
 outcmd=`LANG=C fdisk -l ${DEV_SOURCE}`
 echo "${outcmd}"
 #echo "${outcmd} | grep "^Disk""
@@ -72,7 +82,7 @@ fi
 echo
 #print_linebreak
 
-echo "*** This is the DEST disk ***"
+echo "*** DEST disk information (${DEV_DEST}) ***"
 outcmd=`LANG=C fdisk -l ${DEV_DEST}`
 echo "${outcmd}"
 echo
@@ -97,25 +107,13 @@ if [ "${disksize_source}" -gt "${disksize_dest}" ]; then
 	exit 1
 fi
 
-# NO NO NO
-#
-## Make sure that source and dest have the same blocksize
-#blksize_source=`LANG=C fdisk -l ${DEV_SOURCE} | grep "^Units"`
-#blksize_dest=`LANG=C fdisk -l ${DEV_DEST} | grep "^Units"`
-##echo "DBG: DEV_SOURCE ${blksize_source}"
-##echo "DBG: DEV_DEST   ${blksize_dest}"
-#if [ "${blksize_source}" != "${blksize_dest}" ]; then
-#	echo "ERROR: Incompatible block size: source:${blksize_source} dest:${blksize_dest}"
-#	exit 1
-#fi
-
 # Verify that ${DEV_SOURCE} and ${DEV_DEST} disk geometries are compatible
 geom_source=`LANG=C fdisk -l ${DEV_SOURCE} | grep "cylinders$"`
 geom_dest=`LANG=C fdisk -l ${DEV_DEST} | grep "cylinders$"`
 #echo "DBG: DEV_SOURCE ${geom_source}"
 #echo "DBG: DEV_DEST   ${geom_dest}"
 if [ "${geom_source}" != "${geom_dest}" ]; then
-	echo "WARNING: Different source and dest geometries"
+	echo "WARNING: Different geometries on source and dest devices:"
 	echo "  ${DEV_SOURCE}: ${geom_source}"
 	echo "  ${DEV_DEST}: ${geom_dest}"
 	echo
@@ -147,9 +145,9 @@ fi
 
 # TODO: Is partition table size calculated correctly??? (here is calculated as one cylinder)
 parttbl_size=`LANG=C fdisk -l ${DEV_SOURCE} | awk '/^Units/ {print $9}'`
-echo "DBG: parttbl_size=${parttbl_size}"
+#echo "DBG: parttbl_size=${parttbl_size}"
 
-echo "WARNING: this procedure will destroy contents on ${DEV_DEST}"
+echo "WARNING: THIS WILL DESTROY CONTENTS ON ${DEV_DEST}"
 echo -n "Do you want to proceed (YES/no)? "
 read ok
 if [ "${ok}" != "YES" ]; then
@@ -183,10 +181,136 @@ LANG=C fdisk -l ${DEV_DEST}
 
 # TODO: Why do not heads,sectrk change?
 
+# Copy MBR (boot sector???)
+# TODO: How much should I copy to preserve all MBR???
+#dd if=${DEV_SOURCE} of=${DEV_DEST} bs=512 count=1
+
 fi		# if [ "${OPT_CREATE_DEST_MBR} = "true" ]
 
 
+# TODO: Verify partition layout on ${DEV_SOURCE}
+#    + sdy1: (NTFS, WinXP C:) xx GB
+#    + sdy2: (NTFS, WinXP D:) size specified or all remaining space
 
+
+if [ "${OPT_CREATE_DEST_PARTITIONS}" = "true" ]; then
+
+echo "Deleting all partitions on ${DEV_DEST}..."
+outcmd=`LANG=C fdisk -l ${DEV_DEST} | grep "^${DEV_DEST}"`
+#echo "DBG: outcmd=${outcmd}"
+numparts=`echo "${outcmd}" | wc -l`
+fdiskcmd=`echo "${outcmd}" | awk -v dev=${DEV_SOURCE} -v numparts=${numparts} '
+BEGIN	{
+	print ""
+	}
+//	{
+	part_num=substr($1,length(dev)+1)
+	#print "DBG: part_num=" part_num
+	#print ""
+
+	# Build up fdisk commands
+	print "d"
+	if (numparts > 1) print part_num
+
+	numparts--
+	skip
+	}
+END	{
+	print "p"
+	print "w"
+	}
+'`
+#echo "DBG: fdiskcmd=${fdiskcmd}"
+echo ${fdiskcmd} | tr " " "\n" | LANG=C fdisk ${DEV_DEST} >&/dev/null
+
+# TODO: Create partitions on ${DEV_DEST}
+echo "Creating partitions on ${DEV_DEST} as per ${DEV_SOURCE}..."
+outcmd=`LANG=C fdisk -l ${DEV_SOURCE} | grep "^${DEV_SOURCE}"`
+echo "DBG: outcmd=${outcmd}"
+fdiskcmd=`echo "${outcmd}" | awk -v dev=${DEV_SOURCE} -v numparts=0 '
+BEGIN	{
+	print ""
+	}
+//	{
+	part_num=substr($1,length(dev)+1)
+	#print "DBG: part_num=" part_num
+	part_bootable=($2 == "*")
+	#print "DBG: part_bootable=" part_bootable
+	part_start=(part_bootable ? $3 : $2)
+	#print "DBG: part_start=" part_start
+	part_end=(part_bootable ? $4 : $3)
+	#print "DBG: part_end=" part_end
+	part_id=(part_bootable ? $6 : $5)
+	#print "DBG: part_id=" part_id
+	part_system=(part_bootable ? $7 : $6)
+	#print "DBG: part_system=" part_system
+	print ""
+
+	# TODO: Should extend part_end if OPT_XXX
+
+	# Build up fdisk commands
+	print "n"
+	if (part_num >=1 && part_num <= 4) {
+		if (part_system != "Extended") {
+			print "p"
+		} else {
+			print "e"
+		}
+		print part_num
+	} else {
+		print "l"
+	}
+	print part_start
+	print part_end
+
+	if (part_system != "Extended") {
+		print "t"
+		if (numparts > 0) {
+			print part_num
+		}
+		print part_id
+	}
+
+	if (part_bootable) {
+		print "a"
+		print part_num
+	}
+	print ""
+
+	numparts++
+	skip
+	}
+END	{
+	print "p"
+	print "w"
+	}
+'`
+#echo "DBG: DEV_SOURCE heads: ${heads_source}"
+#echo "DBG: DEV_SOURCE sectors/track: ${sectrk_source}"
+#echo "DBG: DEV_SOURCE cylinders: ${cylinders_source}"
+#echo "DBG: fdiskcmd=${fdiskcmd}"
+echo "
+x
+h
+${heads_source}
+s
+${sectrk_source}
+r
+" ${fdiskcmd} | tr " " "\n" 
+#| LANG=C fdisk ${DEV_DEST}
+# >&/dev/null
+
+echo TODO
+exit 0
+
+# Display what happened in the end...
+LANG=C fdisk -l ${DEV_DEST}
+
+set -x 
+
+
+    # ...
+fi		# if [ "${OPT_CREATE_DEST_PARTITIONS}" = "true" ]
 
 set -x
 echo TODO
@@ -194,18 +318,6 @@ exit 0
 
 
 
-# TODO: Verify partition layout on ${DEV_SOURCE}
-#    + sdy1: (NTFS, WinXP C:) xx GB
-#    + sdy2: (NTFS, WinXP D:) size specified or all remaining space
-
-# TODO: Create partitions on ${DEV_DEST}
-if [ "${OPT_CREATE_DEST_PARTITIONS}" = "true" ]; then
-    # ...
-fi		# if [ "${OPT_CREATE_DEST_PARTITIONS}" = "true" ]
-
-# Copy MBR
-# TODO: How much should I copy to preserve all MBR???
-#dd if=${DEV_SOURCE} of=${DEV_DEST} bs=512 count=1
 
 # TODO: Format partitions on ${DEV_DEST}
 
