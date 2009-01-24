c85e3fff226c6bd4d058be2323c64a0d8a149888
gmacario <gmacario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-01-24 06:51:35 +0000

    Start doing some harm to DEV_DEST...
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@7515 23512697-871b-0410-bc7e-d018c10ccd9e

--- do_clone_disk/do_clone_disk.sh
+++ do_clone_disk/do_clone_disk.sh
@@ -42,9 +42,10 @@ fi
 echo "*** This is the SOURCE disk ***"
 outcmd=`LANG=C fdisk -l ${DEV_SOURCE}`
 echo "${outcmd}"
+#echo "${outcmd} | grep "^Disk""
 echo
 # Make sure DEV_SOURCE exists and is not mounted
-if [ `echo ${outcmd} | grep "Disk ${DEV_SOURCE}:" | wc -l` -ne 1 ]; then
+if [ `echo ${outcmd} | grep "^Disk ${DEV_SOURCE}:" | wc -l` -ne 1 ]; then
     echo "ERROR: Device ${DEV_SOURCE} does not exist"
     exit 1
 fi
@@ -61,7 +62,7 @@ outcmd=`LANG=C fdisk -l ${DEV_DEST}`
 echo "${outcmd}"
 echo
 # Make sure DEV_DEST exists and is not mounted
-if [ `echo ${outcmd} | grep "Disk ${DEV_DEST}:" | wc -l` -ne 1 ]; then
+if [ `echo ${outcmd} | grep "^Disk ${DEV_DEST}:" | wc -l` -ne 1 ]; then
     echo "ERROR: Device ${DEV_DEST} does not exist"
     exit 1
 fi
@@ -70,6 +71,20 @@ if [ `mount | grep ${DEV_DEST} | wc -l` -gt 0 ]; then
     mount | grep ${DEV_DEST}
     exit 1
 fi
+
+
+# Make sure that DEV_SOURCE is not bigger than DEV_DEST
+disksize_source=`LANG=C fdisk -l ${DEV_SOURCE} | grep "Disk ${DEV_SOURCE}:" | awk '// {print $5}'`
+disksize_dest=`LANG=C fdisk -l ${DEV_DEST} | grep "Disk ${DEV_DEST}:" | awk '// {print $5}'`
+#echo "DBG: DEV_SOURCE ${disksize_source}"
+#echo "DBG: DEV_DEST   ${disksize_dest}"
+if [ "${disksize_source}" -gt "${disksize_dest}" ]; then
+	echo "ERROR: incompatible disk size: source:${disksize_source} dest:${disksize_dest}"
+	exit 1
+fi
+
+#echo TODO
+#exit 0
 
 # Make sure that source and dest have the same blocksize
 blksize_source=`LANG=C fdisk -l ${DEV_SOURCE} | grep "^Units"`
@@ -117,7 +132,9 @@ if [ "${cylinders_source}" -gt "${cylinders_dest}" ]; then
 	exit 1
 fi
 
-# TODO: Make sure DEV_DEST is empty
+# TODO: Is partition table size calculated correctly???
+parttbl_size=`LANG=C fdisk -l ${DEV_SOURCE} | grep "^Units"`
+echo "DBG: parttbl_size=${parttbl_size}"
 
 echo "WARNING: this procedure will destroy contents on ${DEV_DEST}"
 echo -n "Do you want to proceed (YES/no)? "
@@ -128,8 +145,11 @@ if [ "${ok}" != "YES" ]; then
 fi
 
 # Sanity checks OK, go ahead...
-echo Cloning disk from ${DEV_SOURCE} to ${DEV_DEST}, please wait...
 
+set -x
+
+echo "Wiping partition table on ${DEV_DEST}..."
+dd if=/dev/zero of=${DEV_DEST} bs=512 count=1024
 
 # TODO: Verify partition layout on ${DEV_SOURCE}
 
@@ -137,7 +157,13 @@ echo Cloning disk from ${DEV_SOURCE} to ${DEV_DEST}, please wait...
 #    + sdy1: (NTFS, WinXP C:) xx GB
 #    + sdy2: (NTFS, WinXP D:) size specified or all remaining space
 
-# TODO: Copy MBR
+# Copy MBR
+# TODO: How much should I copy to preserve all MBR???
+dd if=${DEV_SOURCE} of=${DEV_DEST} bs=512 count=1
+
+# TODO: Format partitions on ${DEV_DEST}
+
+echo "Cloning disk from ${DEV_SOURCE} to ${DEV_DEST}, please wait..."
 
 # TODO: Copy all data in source partitions
 #  + Boot WinXP from C:
