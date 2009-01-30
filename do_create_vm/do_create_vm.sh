9cbe59f907eef37b7b61f48cd4bf2d619df40db5
gmacario <gmacario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-01-30 05:51:13 +0000

    Still working on it...
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@7758 23512697-871b-0410-bc7e-d018c10ccd9e

--- do_create_vm/do_create_vm.sh
+++ do_create_vm/do_create_vm.sh
@@ -2,7 +2,7 @@
 
 # Description:	Create a new VMware VM starting from a backup
 
-set -x
+#set -x
 
 VM_BCKDIR=/var/tmp/Backup_VM
 VM_BCKDATE=20090121
@@ -12,25 +12,29 @@ VM_BASELINE=${VM_BCKDATE}-${VM_OLDNAME}
 VM_DESTDIR=${HOME}/tmp
 VM_NAME=WR_PFI-macario
 
-mkdir -p ${VM_DESTDIR}/${VM_NAME} || exit 1
-
 if [ ! -e ${VM_BCKDIR}/${VM_BASELINE} ]; then
     echo "ERROR: Cannot find VM ${VM_BASELINE} under ${VM_BCKDIR}"
 fi
 (cd ${VM_BCKDIR}/${VM_BASELINE}; if [ -e md5sum.txt ]; then
-    md5sum -c md5sum.txt
+    false #md5sum -c md5sum.txt
 else
     echo "ERROR: Checksums of files on ${VM_BCKDIR}/${VM_BASELINE} do not match"
     exit 1
 fi)
+if [ -e ${VM_DESTDIR}/${VM_OLDNAME} ]; then
+    echo "ERROR: ${VM_OLDNAME} already exists under ${VM_DESTDIR}"
+    echo "       Please change ${VM_DESTDIR} or clean its contents"
+fi
+
+mkdir -p ${VM_DESTDIR}/${VM_NAME} || exit 1
 
 # Everything seems OK, now unpack VM_BASELINE
 #
-cmd_cat="cat ${BCK_OLDNAME}.tgz"
-#cmd_cat="cat ${BCK_OLDNAME}.tgz-[0-9][0-9]"
+cmd_cat="cat ${VM_BASELINE}.tgz"
+#cmd_cat="cat ${VM_BASELINE}.tgz-[0-9][0-9]"
 #
-cmd_untar="tar tvz"
-#cmd_untar="tar xvz"
+#cmd_untar="tar tvz"
+cmd_untar="tar xvz"
 
 (cd ${VM_BCKDIR}/${VM_BASELINE} && ${cmd_cat}) | \
 	(cd ${VM_DESTDIR} && ${cmd_untar})
@@ -41,5 +45,6 @@ echo "== Created new VM ${VM_NAME} under ${VM_DESTDIR}"
 echo "   from backup ${VM_BCKDATE} of VM ${VM_BASELINE}"
 
 # TODO: Should change VM name into *.vm"
+# TODO: Move contents from VM_OLDNAME to VM_NAME
 
 # === EOF ===
