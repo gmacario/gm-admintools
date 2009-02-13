4de28df3d541d2b19eca39bb2b0fb984029e4d77
gmacario <gmacario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-02-13 13:39:59 +0000

    Handle more users
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@8219 23512697-871b-0410-bc7e-d018c10ccd9e

--- nis2table/nis2table.sh
+++ nis2table/nis2table.sh
@@ -16,32 +16,42 @@
 # Copyright 2007-2009 Magneti Marelli Electronic Systems - All Rights Reserved
 # =============================================================================
 
+ypcat passwd || exit 1
 ypcat passwd | awk '
 BEGIN	{
-	printf("%s;%s;%s\n", " LASTNAME", "firstname", "username");
+	printf("%s;%s;%s;%s;%s\n", "* LASTNAME", "firstname", "username", "in_MotoGP", "in_MotoGB");
 	FS=":"
 	#
-	lupin_users["angrusso"] = 1;
-	lupin_users["asanna"] = 1;
-	lupin_users["asimmini"] = 1;
-	lupin_users["cavallot"] = 1;
-	lupin_users["cerato"] = 1;
-	lupin_users["desana"] = 1;
-	lupin_users["faghemo"] = 1;
-	lupin_users["ferrara"] = 1;
-	#lupin_users["fmocci"] = 1;
-	lupin_users["ghiazza"] = 1;
-	lupin_users["ginnuzzi"] = 1;
-	lupin_users["macario"] = 1;
-	lupin_users["paolodoz"] = 1;
-	lupin_users["pierri"] = 1;
-	lupin_users["pirra"] = 1;
-	lupin_users["ponchion"] = 1;
-	lupin_users["sartoric"] = 1;
-	lupin_users["serrett"] = 1;
-	lupin_users["sponza"] = 1;
-	lupin_users["stocchino"] = 1;
-	lupin_users["violino"] = 1;
+	FLAG_MOTOGP = 1;
+	FLAG_MOTOGB = 2;
+	#
+	lupin_users["angrusso"]  = "MOTOGP MOTOGB";
+	lupin_users["asanna"]    = "MOTOGP MOTOGB";
+	lupin_users["asimmini"]  = "MOTOGP MOTOGB";
+	lupin_users["cavallot"]  = "MOTOGP MOTOGB";
+	lupin_users["cerato"]    = "MOTOGP MOTOGB";
+	lupin_users["desana"]    = "MOTOGP MOTOGB";
+	lupin_users["faghemo"]   = "MOTOGP MOTOGB";
+	lupin_users["ferrara"]   = "MOTOGP MOTOGB";
+	lupin_users["ghiazza"]   = "MOTOGP MOTOGB";
+	lupin_users["ginnuzzi"]  = "MOTOGP MOTOGB";
+	lupin_users["macario"]   = "MOTOGP MOTOGB";
+	lupin_users["paolodoz"]  = "MOTOGP MOTOGB";
+	lupin_users["pierri"]    = "MOTOGP MOTOGB";
+	lupin_users["pirra"]     = "MOTOGP MOTOGB";
+	lupin_users["ponchion"]  = "MOTOGP MOTOGB";
+	lupin_users["sartoric"]  = "MOTOGP MOTOGB";
+	lupin_users["serrett"]   = "MOTOGP MOTOGB";
+	lupin_users["sponza"]    = "MOTOGP MOTOGB";
+	lupin_users["stocchino"] = "MOTOGP MOTOGB";
+	lupin_users["violino"]   = "MOTOGP MOTOGB";
+	#
+	# New users to be added
+	#lupin_users["fmocci"]    = "MOTOGP MOTOGB";
+	#lupin_users["nparolini"] = "MOTOGP MOTOGB";
+	#lupin_users["ppantaleo"] = "MOTOGP MOTOGB";
+	#lupin_users["agarcea"]   = "MOTOGP MOTOGB";
+	#lupin_users["msanapo"]   = "MOTOGP MOTOGB";
 	}
 //	{
 	#print "DBG: $0=" $0
@@ -50,6 +60,8 @@ BEGIN	{
 	#
 	firstname=gensub(/\ [a-zA-Z]*$/, "", "g", $5);
 	lastname=toupper(gensub(/.*\ /, "", "g", $5));
+	in_motoGP = ((username in lupin_users) && index("MOTOGP", lupin_users[username]) >= 0) ? "Yes" : "No";
+	in_motoGB = ((username in lupin_users) && index("MOTOGB", lupin_users[username]) >= 0) ? "Yes" : "No";
 	#
 	# Fixup macario firstname
 	if (firstname == "Giampaolo") firstname="Gianpaolo";
@@ -58,7 +70,7 @@ BEGIN	{
 	#print "DBG: lastname=" lastname
 	#print "DBG:"
 	#
-	printf("%s;%s;%s\n", lastname, firstname, username);
+	printf("%s;%s;%s;%s;%s\n", lastname, firstname, username, in_MotoGP, in_MotoGB);
 	}
 END	{
 	}
