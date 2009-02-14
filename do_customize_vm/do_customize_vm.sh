5dcf7c525447064986a00eba551dc314cd3c1e5a
gmacario <gmacario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-02-14 09:41:55 +0000

    More debugging
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@8262 23512697-871b-0410-bc7e-d018c10ccd9e

--- do_customize_vm/do_customize_vm.sh
+++ do_customize_vm/do_customize_vm.sh
@@ -7,14 +7,27 @@ set -x
 
 PFI_USER=cerato
 PFI_GROUP=cerato
+NIS_DOMAIN=lupin.venaria.marelli.it
+NIS_SERVERIP=139.128.24.101
 
 # Customize hostname
-#hostname | grep -v -e "WRI_PFI-${PFI_USER}" - && \
+hostname | grep -e "WRI_PFI-${PFI_USER}"
+retval=$?
+#echo DBG:retval=$retval
+if [ -z $retval ]; then
+	echo "INFO: Setting hostname WR_PFI-${PFI_USER}"
 	echo "WR_PFI-${PFI_USER}" >/etc/hostname || exit 1
+fi
 
-# Link to NIS lupin.marelli.it
+# Bind NIS client to NIS_DOMAIN
 which ypwhich || exit 1
-echo "domain lupin.venaria.marelli.it server 139.128.24.101" >>/etc/yp.conf
+ypwhich | grep $NIS_DOMAIN
+retval=$?
+#echo DBG:retval=$retval
+if [ -z $retval ]; then
+	echo "INFO: setting NIS domain $NIS_DOMAIN"
+	echo "domain $NIS_DOMAIN server $NIS_SERVERIP" >>/etc/yp.conf
+fi
 
 # TODO: see wiki
 exit 0
