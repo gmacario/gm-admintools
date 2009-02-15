08aaebdc148b431af876b68b7fb5404684a234cd
gmacario <gmacario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-02-15 10:10:56 +0000

    Sanity checks before running bootgraph.pl
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@8268 23512697-871b-0410-bc7e-d018c10ccd9e

--- bootgraph/runme.sh
+++ bootgraph/runme.sh
@@ -7,6 +7,26 @@ if [ ! -e bootgraph.pl ]; then
 	wget $upstream/scripts/bootgraph.pl
 fi
 
+#zcat /proc/config.gz | grep CONFIG_PRINTK_TIME || exit 1
+
+cat /proc/cmdline | grep "printk.time=1"
+retval=$?
+#echo DBG: retval=$retval
+if [ $retval -ne 0 ]; then
+	echo Please append 'printk.time=1' to kernel bootcmd
+	exit 1
+fi
+
+cat /proc/cmdline | grep "initcall_debug"
+retval=$?
+#echo DBG: retval=$retval
+if [ $retval -ne 0 ]; then
+	echo Please append 'initcall_debug' to kernel bootcmd
+	exit 1
+fi
+
+set -x
+
 dmesg | perl bootgraph.pl >output.svg
 
 # === EOF ===
