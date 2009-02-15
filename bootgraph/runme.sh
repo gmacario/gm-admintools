92f223c255be7302aea7c0d5f779009dde041796
gmacario <gmacario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-02-15 10:32:38 +0000

    More sanity checks
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@8269 23512697-871b-0410-bc7e-d018c10ccd9e

--- bootgraph/runme.sh
+++ bootgraph/runme.sh
@@ -2,14 +2,23 @@
 
 #upstream=http://lxr.linux.no/linux+v2.6.28.5
 upstream=http://ftp.gnu.org/tmp/linux-libre-fsf2_2.6.28/linux-2.6.28
+outfile=output.svg
 
 if [ ! -e bootgraph.pl ]; then
 	wget $upstream/scripts/bootgraph.pl
 fi
 
-#zcat /proc/config.gz | grep CONFIG_PRINTK_TIME || exit 1
+if [ -e /proc/config.gz ]; then
+	zcat /proc/config.gz | grep CONFIG_PRINTK_TIME >/dev/null
+	retval=$?
+	#echo DBG: retval=$retval
+	if [ $retval -ne 0 ]; then
+		echo Please configure kernel with CONFIG_PRINTK_TIME=y
+		exit 1
+	fi
+fi
 
-cat /proc/cmdline | grep "printk.time=1"
+cat /proc/cmdline | grep "printk.time=1" >/dev/null
 retval=$?
 #echo DBG: retval=$retval
 if [ $retval -ne 0 ]; then
@@ -17,7 +26,7 @@ if [ $retval -ne 0 ]; then
 	exit 1
 fi
 
-cat /proc/cmdline | grep "initcall_debug"
+cat /proc/cmdline | grep "initcall_debug" >/dev/null
 retval=$?
 #echo DBG: retval=$retval
 if [ $retval -ne 0 ]; then
@@ -25,8 +34,9 @@ if [ $retval -ne 0 ]; then
 	exit 1
 fi
 
-set -x
+dmesg | perl bootgraph.pl >$outfile || exit 1
+echo INFO: Boot graph saved as $outfile
 
-dmesg | perl bootgraph.pl >output.svg
+exit 0
 
 # === EOF ===
