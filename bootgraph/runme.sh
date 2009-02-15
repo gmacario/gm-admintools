7ee4117aecd70c7eb592d273fc046ee6f9603ffb
gmacario <gmacario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-02-15 10:53:33 +0000

    Implement cross-target analysis
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@8270 23512697-871b-0410-bc7e-d018c10ccd9e

--- bootgraph/runme.sh
+++ bootgraph/runme.sh
@@ -2,40 +2,70 @@
 
 #upstream=http://lxr.linux.no/linux+v2.6.28.5
 upstream=http://ftp.gnu.org/tmp/linux-libre-fsf2_2.6.28/linux-2.6.28
-outfile=output.svg
+
+# Work directory
+workdir=`hostname`
+#workdir=target
+#
+# Uncomment if working across target (data should exist in workdir)
+#OPT_CROSS_TARGET=true
+#
+# Output file
+outfile=$workdir/output.svg
+
+echo "INFO: bootgraph v0.1"
 
 if [ ! -e bootgraph.pl ]; then
 	wget $upstream/scripts/bootgraph.pl
 fi
 
-if [ -e /proc/config.gz ]; then
-	zcat /proc/config.gz | grep CONFIG_PRINTK_TIME >/dev/null
+if [ "$OPT_CROSS_TARGET" = "true" ]; then
+	echo INFO: Taking boot time information from directory $workdir
+else
+	echo INFO: Saving boot time information to directory $workdir
+	mkdir -p $workdir || exit 1
+	if [ -e /proc/config.gz ]; then
+		zcat /proc/config.gz >$workdir/config.txt
+	else
+		echo WARNING: Make sure that kernel is built with CONFIG_PRINTK_TIME=y
+	fi
+	cat /proc/cmdline >$workdir/cmdline.txt
+	dmesg >$workdir/dmesg.txt
+fi
+
+if [ ! -e $workdir ]; then
+	echo ERROR: work directory $workdir does not exist
+	exit 1
+fi
+
+if [ -e $workdir/config.txt ]; then
+	grep -e CONFIG_PRINTK_TIME $workdir/config.txt >/dev/null
 	retval=$?
 	#echo DBG: retval=$retval
 	if [ $retval -ne 0 ]; then
-		echo Please configure kernel with CONFIG_PRINTK_TIME=y
+		echo ERROR: Please configure kernel with CONFIG_PRINTK_TIME=y
 		exit 1
 	fi
 fi
 
-cat /proc/cmdline | grep "printk.time=1" >/dev/null
+grep "printk.time=1" $workdir/cmdline.txt >/dev/null
 retval=$?
 #echo DBG: retval=$retval
 if [ $retval -ne 0 ]; then
-	echo Please append 'printk.time=1' to kernel bootcmd
+	echo "ERROR: Please append 'printk.time=1' to kernel bootcmd"
 	exit 1
 fi
 
-cat /proc/cmdline | grep "initcall_debug" >/dev/null
+grep "initcall_debug" $workdir/cmdline.txt >/dev/null
 retval=$?
 #echo DBG: retval=$retval
 if [ $retval -ne 0 ]; then
-	echo Please append 'initcall_debug' to kernel bootcmd
+	echo "ERROR: Please append 'initcall_debug' to kernel bootcmd"
 	exit 1
 fi
 
-dmesg | perl bootgraph.pl >$outfile || exit 1
-echo INFO: Boot graph saved as $outfile
+cat $workdir/dmesg.txt | perl bootgraph.pl >$outfile || exit 1
+echo "INFO: Boot graph saved as $outfile"
 
 exit 0
 
