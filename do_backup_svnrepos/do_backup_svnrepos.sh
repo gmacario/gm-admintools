4084872f6d211388ab59614a9623e860775e8afd
gmacario <gmacario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-03-01 10:31:21 +0000

    Backup svnrepos inside BACKUPDIR
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@8673 23512697-871b-0410-bc7e-d018c10ccd9e

--- do_backup_svnrepos/do_backup_svnrepos.sh
+++ do_backup_svnrepos/do_backup_svnrepos.sh
@@ -4,12 +4,16 @@
 # =============================================================================
 
 NOW=`date '+%Y%m%d-%H%M'`
-REMOTE=macario@inno05.venaria.marelli.it
+
+REMOTEUSER=macario
+REMOTEHOST=inno05.venaria.marelli.it
 
 REPOSITORIES=""
 REPOSITORIES+=" inno"
 REPOSITORIES+=" mmseti"
 REPOSITORIES+=" osstbox"
+
+BACKUPDIR=/backup/Backup_svnrepos/`date '+%Y%m%d'`-inno05
 
 #set -x
 
@@ -20,16 +24,20 @@ REPOSITORIES+=" osstbox"
 #	svnadmin: Can't read length line in file '/opt/repos/mmseti/db/revs/6'
 #
 
+echo "INFO: $0 - v0.1"
+
+mkdir -p $BACKUPDIR || exit 1
+cd $BACKUPDIR || exit 1
 for repos in ${REPOSITORIES}; do
-    echo "INFO: Dumping SVN repos $repos..."
-    (ssh ${REMOTE} svnadmin dump /opt/repos/$repos | gzip -c -9) \
-	| split -b 1024m -d - ${NOW}-bk-$repos.svndump.gz-split
+    echo "INFO: Dumping SVN repos $repos from $REMOTEHOST..."
+    (ssh ${REMOTEUSER}@${REMOTEHOST} svnadmin dump /opt/repos/$repos | gzip -c -9) \
+	| split -b 2048m -d - ${NOW}-bk-$repos.svndump.gz-split
     retval=$?
     if [ $retval -ne 0 ]; then
-	echo "ERROR dumping repository $repos";
+	echo "ERROR: Dumping repository $repos returned $retval";
 	exit 1
     fi
 done
-echo "INFO: Dumping repositories completed"
+echo "INFO: Dumping repositories to $BACKUPDIR completed"
 
 # === EOF ===
