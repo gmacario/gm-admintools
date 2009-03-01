f67c3de339ae93327293b9039a8db1ef22010245
gmacario <gmacario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-03-01 08:42:23 +0000

    Sanity check of CUSTOMER variable
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@8664 23512697-871b-0410-bc7e-d018c10ccd9e

--- do_censore_vm/do_censore_vm.sh
+++ do_censore_vm/do_censore_vm.sh
@@ -21,14 +21,20 @@
 # Copyright 2007-2009 Magneti Marelli Electronic Systems - All Rights Reserved
 # =============================================================================
 
-# CUSTOMER should be chosen between one of those:
-#	BMW		BMW (EntryNav,NBT)
-#	GENIVI		GENIVI (members with access to BMWI-POSR)
-#	LUPIN		LUPIN team
-#	MMSE		Magneti Marelli (outside the LUPIN team)
-#	PSA		Peugeot-Citroen (SMEG Customer)
-#	TES		TES (Contractor for OpenGL to SMEG/Linux)
+# CUSTOMER should be chosen within KNOWN_CUSTOMERS (see below)
 CUSTOMER=clueless
+
+# -----------------------------------------------------------------------------
+# You should not need to change the script below
+# -----------------------------------------------------------------------------
+
+KNOWN_CUSTOMERS=""
+KNOWN_CUSTOMERS+="BMW "		# BMW (EntryNav,NBT)
+KNOWN_CUSTOMERS+="GENIVI "	# GENIVI (members with access to BMWI-POSR)
+KNOWN_CUSTOMERS+="LUPIN "	# LUPIN team
+KNOWN_CUSTOMERS+="MMSE "	# Magneti Marelli (outside the LUPIN team)
+KNOWN_CUSTOMERS+="PSA "		# Peugeot-Citroen (SMEG Customer)
+KNOWN_CUSTOMERS+="TES "		# TES (Contractor for OpenGL to SMEG/Linux)
 
 # -----------------------------------------------------------------------------
 # The script must handle the following directories:
@@ -55,22 +61,32 @@ CUSTOMER=clueless
 #   + license
 
 # -----------------------------------------------------------------------------
-# You should not need to change the script below
-# -----------------------------------------------------------------------------
-
-# -----------------------------------------------------------------------------
 # Main Program starts here
 #
 echo "INFO: $0 - v0.1"
-echo "INFO: Censoring VM for CUSTOMER=$CUSTOMER"
 
-if [ "$CUSTOMER" == "" -or "$CUSTOMER" == "clueless" ]; then
+if [ "$CUSTOMER" = "" ]; then
 	echo "ERROR: Should configure CUSTOMER within script"
 	exit 1
 fi
 
+ok=1
+#echo "DBG: KNOWN_CUSTOMERS=\"$KNOWN_CUSTOMERS\""
+for cust in $KNOWN_CUSTOMERS; do
+	#echo "DBG: cust=$cust";
+	[ "$CUSTOMER" = "$cust" ] && ok=0
+done
+if [ $ok -eq 1 ]; then
+	echo "ERROR: CUSTOMER=$CUSTOMER is not one of: $KNOWN_CUSTOMERS"
+	exit 1
+fi
+
+echo "INFO: Censoring VM for CUSTOMER=$CUSTOMER"
+
 # TODO TODO TODO
 set -x
+
+exit 1
 
 if [ $CUSTOMER != LUPIN ]; then
 	rm -rf /opt/LUPIN/code/trunk/area_51
