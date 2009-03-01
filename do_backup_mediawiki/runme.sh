3c6b70111a3d3e8da9547a6e86b2ac7e3d0b3843
gmacario <gmacario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-03-01 07:38:01 +0000

    Backup wiki images as well
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@8657 23512697-871b-0410-bc7e-d018c10ccd9e

--- do_backup_mediawiki/runme.sh
+++ do_backup_mediawiki/runme.sh
@@ -7,8 +7,20 @@ REMOTEUSER=macario
 REMOTEHOST=inno10.venaria.marelli.it
 REMOTEDIR=/home/macario/do_backup_mediawiki
 
+echo "INFO: $0 v0.1"
+
+# Backup MySQL
+echo "INFO: Creating backup of MySQL DB from $REMOTEHOST"
 mkdir -p $BACKUPDIR || exit 1
-ssh $REMOTEUSER@$REMOTEHOST "(cd $REMOTEDIR && ./automysqlbackup_inno10.sh)"
-scp -r $REMOTEUSER@$REMOTEHOST:$REMOTEDIR/mysql_backups $BACKUPDIR
+ssh $REMOTEUSER@$REMOTEHOST "(cd $REMOTEDIR && ./automysqlbackup_inno10.sh)" || exit 1
+
+echo "INFO: Copying backup of MySQL DB from $REMOTEHOST"
+scp -r $REMOTEUSER@$REMOTEHOST:$REMOTEDIR/mysql_backups $BACKUPDIR || exit 1
+
+# Backup Images
+for wiki in innowiki nbtwiki osstbox; do
+	echo "INFO: Backing up images from $wiki at $REMOTEHOST"
+	scp -r $REMOTEUSER@$REMOTEHOST:/var/www/$wiki/images $BACKUPDIR/$wiki || exit 1
+done
 
 # === EOF ===
