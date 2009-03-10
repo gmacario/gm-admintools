eb1eaf3d31ead38554145bdce7ed54b7e6e40d83
gmacario <gmacario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-03-10 16:58:55 +0000

    Handle case of passwd_crypt=xxx
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@8858 23512697-871b-0410-bc7e-d018c10ccd9e

--- nis_utils/nis_lock_default_passwd.sh
+++ nis_utils/nis_lock_default_passwd.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+#set -x
+
 for username in `ypcat passwd | awk '
 BEGIN	{ FS = ":" }
-/GzslqrxRRWFfE/	{ print $1 }
+($2 == "GzslqrxRRWFfE")	{ print $1 }
+($2 == "xxx")	{ print $1 }
 '`; do
 	#echo "DBG:username=$username"
 	if [ "$username" != "rodino" ]; then
@@ -12,7 +15,7 @@ BEGIN	{ FS = ":" }
 		#sudo yppasswd -p $username
 		#su -c passwd $username
 		#su -c yppasswd $username
-		sudo ./nis_set_passwd.sh $username xxx
+		sudo ./nis_set_passwd.sh $username xxx >/dev/null
 	fi
 done
 
