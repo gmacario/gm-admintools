26ef0b4f184043d542ed534830985a9e5bfa4213
macario <macario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-03-31 05:09:36 +0000

    Updated script to create .authz file for lupin05
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@9275 23512697-871b-0410-bc7e-d018c10ccd9e

--- migrate_svnusers/migrate_snvusers.sh
+++ migrate_svnusers/migrate_snvusers.sh
@@ -17,45 +17,116 @@ fi
 
 awk '
 BEGIN	{
-	#transtable["olduser"] = "newuser";
 	#transtable[""] = "";
-	transtable["mstocchino"] = "stocchino";
-	transtable["pdoz"] = "paolodoz";
-	transtable["gmacario"] = "macario";
-	transtable["gpirra"] = "pirra";
-	transtable["cbesozzi"] = "besozzi";
-	transtable["ebilliet"] = "billiet";
-	transtable["gcasanova"] = "casanova";
-	transtable["pcavallotti"] = "cavallot";
-	transtable["acerato"] = "cerato";
-	transtable["adamiano"] = "damiano";
-	transtable["ddesana"] = "desana";
-	transtable["sfarina"] = "farina";
-	transtable["rferrara"] = "ferrara";
-	transtable["lfresi"] = "fresi";
-	transtable["mghiazza"] = "ghiazza";
-	transtable["mmarcia"] = "marcia";
-	transtable["mnoberasco"] = "noberasco";
-	transtable["gnuzzi"] = "ginnuzzi";
-	transtable["nparolini"] = "parolini";
-	transtable["mpierri"] = "pierri";
-	transtable["gponchione"] = "ponchion";
-	transtable["arusso"] = "angrusso";
-	transtable["eserretti"] = "serrett";
-	transtable["wviolino"] = "violino";
-	transtable["csponza"] = "sponza";
-	transtable["elosego"] = "losego";
-	transtable["eturpault"] = "turpault";
-	transtable["mturinetto"] = "turinett";
-	transtable["ppinna"] = "pinna";
-	transtable["rherard"] = "herard";
-	transtable["yclemenceau"] = "clemence";
 
-	# TODO
+	# Translate MMSE (employees and contractors) users
+	#           old_user              new_user
+	# -------------------------------------------------
+	#transtable["faghemo"] = "faghemo";
+	transtable["tallaoua"]         = "allaoua";
+	#transtable["?"] = "santonel";
+	#transtable["fbasso"] = "fbasso";
+	transtable["cbesozzi"]         = "besozzi";
+	transtable["ebilliet"]         = "billiet";
+	#transtable["sbosco"] = "sbosco";
+	#transtable["ebruci"] = "ebruci";
+	#transtable["lbuso"] = "lbuso";
+	#transtable["?"] = "calabro";
+	transtable["ccambursano"]      = "cambursa";
+	transtable["gcasanova"]        = "casanova";
+	transtable["pcavallotti"]      = "cavallot";
+	transtable["acerato"]          = "cerato";
+	#transtable["?"] = "cingolan";
+	transtable["yclemenceau"]      = "clemence";
+	#transtable["?"] = "copetti";
+	transtable["mcurti"] = "curti";
+	transtable["adamiano"]         = "damiano";
+	transtable["ddesana"]          = "desana";
+	transtable["pdoz"]             = "paolodoz";
+	#transtable["?"] = "faggiani";
+	transtable["sfarina"]          = "farina";
+	transtable["rferrara"]         = "ferrara";
+	transtable["lfresi"]           = "fresi";
+	#transtable["?"] = "garberog";
+	transtable["agarcea"]          = "garcea";
+	#transtable["?"] = "gennarin";
+	transtable["mghiazza"]         = "ghiazza";
+	transtable["ggiacoia"]         = "giacoia";
+	#transtable["?"] = "gilforte";
+	transtable["ggiraudo"]         = "giraudo";
+	transtable["egraglia"] = "graglia";
+	#transtable["?"] = "grellier";
+	transtable["rherard"]          = "herard";
+	#transtable["pleone"] = "pleone";
+	#transtable["?"] = "lococo";
+	transtable["elosego"]          = "losego";
+	transtable["gmacario"]         = "macario";
+	transtable["amaddau"]          = "maddau";
+	transtable["mmarcia"]          = "marcia";
+	#transtable["rmarino"] = "rmarino";
+	#transtable["martorel"] = "martorel";
+	transtable["fmocci"]           = "mocci";
+	transtable["amurru"]           = "murru";
+	transtable["mnassi"]           = "nassi";
+	transtable["mnoberasco"]       = "noberasco";
+	transtable["gnuzzi"]           = "ginnuzzi";
+	#transtable["sorlandi"] = "sorlandi";
+	transtable["cpalma"]           = "copalma";
+	transtable["dpanero"]          = "panero";
+	transtable["ppantaleo"]        = "ppantale";
+	transtable["lparenti"]         = "parenti";
+	transtable["nparolini"]        = "parolini";
+	transtable["lpicerno"]         = "picerno";
+	transtable["mpierri"]          = "pierri";
+	#transtable["epinna"] = "epinna";
+	transtable["ppinna"]           = "pinna";
+	transtable["gpirra"]           = "pirra";
+	transtable["gponchione"]       = "ponchion";
+	transtable["pporcu"]           = "porcu";
+	#transtable["?"] = "re";
+	#transtable["mrocca"] = "mrocca";
+	#transtable["?"] = "rodino";
+	transtable["arusso"]           = "angrusso";
+	#transtable["fsalerno"] = "fsalerno";
+	transtable["msanapo"]          = "sanapo";
+	#transtable["asanna"] = "asanna";
+	transtable["asannio"]          = "sannio";
+	#transtable["?"] = "sartoric";
+	#transtable["?"] = "serre";
+	transtable["eserretti"]        = "serrett";
+	#transtable["asimmini"] = "asimmini";
+	transtable["msoddu"]           = "soddu";
+	transtable["csponza"]          = "sponza";
+	transtable["mstocchino"]       = "stocchino";
+	#transtable["?"] = "strazzullo";
+	#transtable["?"] = "terree";
+	#transtable["ftosetto"] = "?";
+	transtable["mturinetto"]       = "turinett";
+	transtable["eturpault"]        = "turpault";
+	#transtable["?"] = "tuttobene";
+	transtable["wviolino"] = "violino";
+	transtable["rzompi"]           = "zompi";
+
+	# TODO: Translate non-MMSE users
+	#           old_user              new_user
+	# -------------------------------------------------
+	#transtable["lbelluccini"] = "?";
+	#transtable["rlupin"] = "?";
+	#transtable["fpagin"] = "?";
+	#transtable["mtorchiano"] = "?";
+	#transtable["mviolante"] = "?";
 	
-	#transtable["oldrepos"] = "newrepos";
-	transtable["osstbox"] = "osstlab";
-	# TODO
+	# Translate repository names
+	#           old_repos             new_repos
+	# -------------------------------------------------
+	transtable["inno:"]            = "lupin:";
+	transtable["osstbox:"]         = "osstlab:";
+
+	# Translate hostnames
+	#           old_hostname          new_hostname
+	# -------------------------------------------------
+	transtable["inno05"]           = "lupin05";
+
 	}
 //	{
 	for (word in transtable) {
@@ -65,6 +136,9 @@ BEGIN	{
 	}
 ' ${oldauth_file} >dav_svn.authz
 
-diff -u $oldauth_file dav_svn.authz
+set -x
+
+#diff -u $oldauth_file dav_svn.authz
+diff -y -W 165 $oldauth_file dav_svn.authz |less
 
 # === EOF ===
