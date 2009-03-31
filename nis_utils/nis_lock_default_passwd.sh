480928de987706172db31a14b35689155cacf697
macario <macario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-03-31 05:12:00 +0000

    For debug, actual password locking is commented out
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@9276 23512697-871b-0410-bc7e-d018c10ccd9e

--- nis_utils/nis_lock_default_passwd.sh
+++ nis_utils/nis_lock_default_passwd.sh
@@ -5,17 +5,13 @@
 for username in `ypcat passwd | awk '
 BEGIN	{ FS = ":" }
 ($2 == "GzslqrxRRWFfE")	{ print $1 }
-($2 == "xxx")	{ print $1 }
+#($2 == "xxx")	{ print $1 }
+($2 == "yyy")	{ print $1 }
 '`; do
 	#echo "DBG:username=$username"
 	if [ "$username" != "rodino" ]; then
-		echo "DBG: Locking password for user $username"
-		#sudo passwd $username
-		#sudo su -c passwd $username
-		#sudo yppasswd -p $username
-		#su -c passwd $username
-		#su -c yppasswd $username
-		sudo ./nis_set_passwd.sh $username xxx >/dev/null
+		echo "TODO: Lock password for user $username"
+		##TODO: sudo ./nis_set_passwd.sh $username xxx >/dev/null
 	fi
 done
 
