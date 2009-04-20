177da62f32148fc9cf1c49b488959db79d74b9fd
macario <macario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-04-20 05:00:31 +0000

    First attempt at backup of last modified files
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@9500 23512697-871b-0410-bc7e-d018c10ccd9e

---
+++ lastchanged/lastchanged.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -x
+set -e
+
+REMOTEUSER=ubuntu
+REMOTEHOST=192.168.82.130
+BASEDIR=/media/win_d
+#BASEDIR=/tmp
+FIND_PATTERN="-mtime -7"
+
+LIST=lastchanged.txt
+TARBALL=lastchanged.tar.gz
+
+if [ ! -e $LIST ]; then
+	ssh $REMOTEUSER@$REMOTEHOST "cd $BASEDIR && find . $FIND_PATTERN" >$LIST
+	echo "INFO: Saving list from $LIST"
+	exit
+else
+	echo "INFO: Backing up files from $LIST"
+fi
+
+scp $LIST $REMOTEUSR@$REMOTEHOST:/tmp
+ssh $REMOTEUSER@$REMOTEHOST "cd $BASEDIR && tar cvz $(cat /tmp/$LIST)" >$TARBALL
+
+# === EOF ===
