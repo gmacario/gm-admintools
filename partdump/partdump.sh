b15f783707f1d0d2d557e49c08e5183d7d4f26ee
macario <macario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-05-05 21:27:51 +0000

    Supports writing files to OUTDIR
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@9719 23512697-871b-0410-bc7e-d018c10ccd9e

--- partdump/partdump.sh
+++ partdump/partdump.sh
@@ -1,9 +1,38 @@
 #!/bin/sh
 
-set -x -e
+export SSD_DEV=/dev/sdb
+export PARTITIONS="1 2 3 4"
+export MOUNTPOINT=/tmp/mount
+export OUTDIR=/home/gmacario/MOVEME/20090503-Russelville_SSD
+export LOGFILE=partdump.log
 
-#[ `whoami` != root ] && exit 1
+#set -x
+set -e
 
-sudo ./runme2.sh 2>&1 | tee partdump.log
+if [ `whoami` != root ]; then
+	echo "ERROR: This program must be run as root"
+	exit 1
+fi
+
+runme2()
+{
+fdisk -l ${SSD_DEV} >$OUTDIR/fdisk.txt
+
+for part in $PARTITIONS; do
+    umount ${SSD_DEV}$part || true
+done
+
+dd if=$SSD_DEV | gzip -c -9 >$OUTDIR/raw_device.gz
+
+for part in $PARTITIONS; do
+    mkdir -p $MOUNTPOINT
+    mount -o ro ${SSD_DEV}$part $MOUNTPOINT
+    (cd $MOUNTPOINT && tar cz .) >$OUTDIR/part$part.tar.gz
+    umount $MOUNTPOINT
+done
+}
+
+#sudo ./runme2.sh 2>&1 | tee $OUTDIR/$LOGFILE
+runme2 2>&1 | tee $OUTDIR/$LOGFILE
 
 # === EOF ===
