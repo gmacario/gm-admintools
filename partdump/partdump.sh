a0d4eba2aa99e45a6c1eb3d275dfb628711194cc
macario <macario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-05-05 21:47:46 +0000

    Code cleanup, add comments
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@9720 23512697-871b-0410-bc7e-d018c10ccd9e

--- partdump/partdump.sh
+++ partdump/partdump.sh
@@ -1,25 +1,43 @@
 #!/bin/sh
 
-export SSD_DEV=/dev/sdb
-export PARTITIONS="1 2 3 4"
-export MOUNTPOINT=/tmp/mount
-export OUTDIR=/home/gmacario/MOVEME/20090503-Russelville_SSD
-export LOGFILE=partdump.log
+# =============================================================================
+# Project:	LUPIN
+#
+# Purpose:	Dump hard disk partitions
+#
+# Language:	GNU shell script
+#
+# Copyright 2007-2009 Magneti Marelli Electronic Systems - All Rights Reserved
+# =============================================================================
 
+# Configurable Parameters
+#
+SSD_DEV=/dev/sdb
+PARTITIONS="1 2 3 4"
+MOUNTPOINT=/tmp/mount
+OUTDIR=/home/gmacario/MOVEME/20090503-Russelville_SSD
+LOGFILE=partdump.log
+
+# End of configurable parameters
+
+# -----------------------------------------------------------------------------
 #set -x
 set -e
 
+runme2()
+{
 if [ `whoami` != root ]; then
 	echo "ERROR: This program must be run as root"
 	exit 1
 fi
 
-runme2()
-{
+echo "INFO: Dumping partitions of ${SSD_DEV} into ${OUTDIR}"
+echo "INFO: Dump started at `date`"
+
 fdisk -l ${SSD_DEV} >$OUTDIR/fdisk.txt
 
 for part in $PARTITIONS; do
-    umount ${SSD_DEV}$part || true
+    umount ${SSD_DEV}$part 2>/dev/null || true
 done
 
 dd if=$SSD_DEV | gzip -c -9 >$OUTDIR/raw_device.gz
@@ -30,9 +48,9 @@ for part in $PARTITIONS; do
     (cd $MOUNTPOINT && tar cz .) >$OUTDIR/part$part.tar.gz
     umount $MOUNTPOINT
 done
+echo "INFO: Dump completed at `date`"
 }
 
-#sudo ./runme2.sh 2>&1 | tee $OUTDIR/$LOGFILE
 runme2 2>&1 | tee $OUTDIR/$LOGFILE
 
 # === EOF ===
