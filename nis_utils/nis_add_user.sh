3bb2b7baced9588b4d31c01ddb359d4c0995c736
macario <macario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-05-10 08:03:16 +0000

    Fix homedir in NIS entry. Cosmetic improvements
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@9867 23512697-871b-0410-bc7e-d018c10ccd9e

--- nis_utils/nis_add_user.sh
+++ nis_utils/nis_add_user.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 #set -x
+set -e
 
 if [ $# -lt 2 ]; then
 	echo "Usage: $0 username displayname [newpass_crypt]"
@@ -10,9 +11,10 @@ username=$1
 displayname=$2
 newpass_crypt=$3
 
-ypcat passwd | grep -v -e "^$username:" >/dev/null
-retval=$?
-if [ $retval -ne 0 ]; then
+#set -x
+f_dupe=`ypcat passwd | grep -e "^$username:" | wc -l` >/dev/null
+#echo "DBG: f_dupe=$f_dupe"
+if [ $f_dupe -gt 0 ]; then
     echo "ERROR: Username $username already present in NIS database"
     exit 1
 fi
@@ -28,9 +30,12 @@ fi
 cd /var/yp/maps || exit 1
 cp passwd passwd.OLD || exit 1
 
-echo "INFO: Creating new NIS entry (username:$username; uid=$last_uid; displayname:$displayname)"
+echo "INFO: Creating new NIS entry:"
+echo "INFO:     username:$username"
+echo "INFO:     uid=$last_uid"
+echo "INFO:     displayname:$displayname"
 
-# Entry with uid=last_uid is the template to duplicate
+# Entry with (uid == last_uid) is the template to duplicate
 
 awk \
 -v "username=$username" \
@@ -39,27 +44,34 @@ awk \
 -v "last_uid=$last_uid" \
 '
 BEGIN	{
-	FS=":"
-	OFS=":"
+	FS=":";
+	OFS=":";
 	}
 $3 == last_uid {
-	sample_entry=$0
+	sample_entry=$0;
 	next;
 	}
 //	{
-	print $0
+	print $0;
 	}
 END	{
-	# New Entry
+	# Append new_entry (modeled after sample_entry)
 	$0=sample_entry;
-	$5=displayname;
-	$2=newpass_crypt;
+	sample_username=$1;
+	#
 	$1=username;
+	$2=newpass_crypt;
+	# Keep $3 (uid) as in sample_entry
+	# Keep $4 (gid) as in sample_entry
+	$5=displayname;
+	# Adjust $6 (homedir)
+	gsub(sample_username, username, $6);
+	# Keep $6 (default_shell) as in sample_entry
 	print $0;
 	#
-	# Sample Entry
+	# Leave sample_entry at end (increment uid)
 	$0=sample_entry;
-	$3=$3+1
+	$3=$3+1;
 	print $0;
 	}
 ' passwd.OLD >passwd || exit 1
@@ -67,23 +79,21 @@ END	{
 cd /var/yp || exit 1
 make >/dev/null || exit 1
 
-#if [ "$2" = "" ]; then
-#    echo "INFO: Changing password for user $user"
-#    sudo -u $user yppasswd
-#else
-#    echo "INFO: Changed password for user $user"
-#    ypcat passwd | grep "^$user:"
-#fi
+echo "INFO: Created entry in NIS:"
+ypcat passwd | grep "^${username}:"
 
-set -x
+#set -x
 
 gid=`ypcat passwd | grep "^${username}:" | cut -d ':' -f 4`
-echo "DBG: gid=${gid}"
+#echo "DBG: gid=${gid}"
 
 # Create and populate Home Directory for new username
 mkdir /home/${username} || exit 1
 [ -e /etc/skel ] && (cd /etc/skel && cp -a . /home/${username})
 chown -R ${username}.${gid} /home/${username} || exit 1
+
+echo "INFO: Created user homedir (/home/${username}):"
+ls -la /home/${username}
 
 exit 0
 
