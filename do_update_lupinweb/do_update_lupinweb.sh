8f829ce1770e12eb5a0056639389fd566fbd3aa1
macario <macario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-05-18 08:07:14 +0000

    Parameterize script
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@10131 23512697-871b-0410-bc7e-d018c10ccd9e

--- do_update_lupinweb/do_update_lupinweb.sh
+++ do_update_lupinweb/do_update_lupinweb.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+#
+# Description:	Helper script to deploy a web application
 
-ssh user01@lupin08.venaria.marelli.it "cd /var/www/lupin-web && svn update"
+REMOTEUSER=user01
+REMOTESITE=lupin08.venaria.marelli.it
+BASEDIR=/var/www/lupin-web
+REVISION=HEAD
+
+#ssh user01@lupin08.venaria.marelli.it "cd /var/www/lupin-web && svn update"
+ssh ${REMOTEUSER}@${REMOTESITE} "cd ${BASEDIR} && svn update -r${REVISION}"
 
 # === EOF ===
