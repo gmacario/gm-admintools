715878b13db25e1fd480225a4ac5a8c84ba4465a
macario <macario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-07-03 05:08:52 +0000

    Successfully tested on lxdemo01
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@11449 23512697-871b-0410-bc7e-d018c10ccd9e

--- vmware_utils/vm_create_image.sh
+++ vmware_utils/vm_create_image.sh
@@ -25,22 +25,18 @@
 # Configurable Parameters
 #
 # NOTE: The following configuration variables
-#       may be overridden by do_create_vm.conf (see comments above)
+#       may be overridden by vm_create_image.conf (see comments above)
 
-#??? VM_BCKDIR=/var/tmp/Backup_VM
-#VM_BCKDATE=20090201
-#VM_OLDNAME=Ubuntu804-WR_PFIjan28
-
-#VM_DESTDIR=${HOME}/tmp
-VM_DESTDIR="/var/lib/vmware/Virtual Machines"
+VM_DESTDIR=${HOME}/tmp
+#VM_DESTDIR="/var/lib/vmware/Virtual Machines"
 #
-VM_NAME=Ubuntu904beta-test01
+VM_NAME=Ubuntu904-test01
 VM_TEMPLATE=samples/Ubuntu.vmx
 
 VMRUN_SERVERTYPE=server
-VMRUN_SERVERHOST=https://lupin01.venaria.marelli.it:8333/sdk
-VMRUN_USER=macario
-#VMRUN_PASS=xxxx
+VMRUN_SERVERHOST=https://localhost:8333/sdk
+#VMRUN_USER=myuser
+#VMRUN_PASS=mysecret
 
 # -----------------------------------------------------------------------------
 # You should not need to change the script below
@@ -56,7 +52,7 @@ echo "INFO: $0 - v0.2"
 #
 confpattern="vm_create_image"
 confpath=""
-if [ -e ./$conpattern.conf ]; then
+if [ -e ./$confpattern.conf ]; then
     confpath=./$confpattern.conf
 elif [ -e ${HOME}/.$confpattern/$confpattern.conf ]; then
     confpath=${HOME}/.$confpattern/$confpattern.conf
@@ -219,66 +215,6 @@ set -x
 # 	/bin/ls -la /
 
 # -----------------------------------------------------------------------------
-
-#VM_BASELINE=${VM_BCKDATE}-${VM_OLDNAME}
-#
-#if [ ! -e ${VM_BCKDIR}/${VM_BASELINE} ]; then
-#    echo "ERROR: Cannot find VM ${VM_BASELINE} under ${VM_BCKDIR}"
-#    exit 1
-#fi
-#if [ -e ${VM_DESTDIR}/${VM_OLDNAME} ]; then
-#    echo "ERROR: ${VM_OLDNAME} already exists under ${VM_DESTDIR}"
-#    echo "INFO: Please change ${VM_DESTDIR} or clean its contents"
-#    exit 1
-#fi
-
-##cmd_cat="cat ${VM_BASELINE}.tgz"
-#cmd_cat="cat ${VM_BASELINE}.tgz-[0-9][0-9]"
-##
-##cmd_untar="tar tvz"
-#cmd_untar="tar xvz"
-
-#cd ${VM_BCKDIR}/${VM_BASELINE}
-#if [ -e md5sum.txt ]; then
-#    echo "INFO: Verifying backup file checksums..."
-#    md5sum -c md5sum.txt || exit 1
-#else
-#    echo "WARNING: No md5sum.txt in ${VM_BCKDIR}/${VM_BASELINE}"
-#fi
-
-#echo "INFO: Extracting backup into ${VM_DESTDIR}..."
-#mkdir -p ${VM_DESTDIR}/${VM_OLDNAME}
-#retval=$?
-#if [ $retval -ne 0 ]; then
-#        echo "ERROR: Cannot create directory under ${VM_DESTDIR}"
-#        exit 1
-#fi
-#(cd ${VM_BCKDIR}/${VM_BASELINE} && ${cmd_cat}) | \
-#	(cd ${VM_DESTDIR} && ${cmd_untar})
-#retval=$?
-#if [ $retval -ne 0 ]; then
-#        echo "ERROR: Problems extracting files from ${VM_BASELINE}"
-#        exit 1
-#fi
-#
-## TODO: Should verify that rename of displayname works in all cases...
-#
-#cd ${VM_DESTDIR}
-#if [ "${VM_OLDNAME}" != "${VM_NAME}" ]; then
-#    mv "${VM_OLDNAME}" "${VM_NAME}"
-#    cd "${VM_NAME}"
-#    for file in *.vmx; do
-#	cp $file $file.ORIG
-#	awk -v vm_name="${VM_NAME}" '
-#/^display[Nn]ame/	{printf("displayName = \"%s\"\n", vm_name);
-#			next }
-#//			{print $0}
-#' $file.ORIG >$file
-#    done
-#fi
-#
-#echo "INFO: Created new VM ${VM_NAME} under ${VM_DESTDIR}"
-#echo "INFO: from backup ${VM_BCKDATE} of VM ${VM_BASELINE}"
-
+# TODO
 
 # === EOF ===
