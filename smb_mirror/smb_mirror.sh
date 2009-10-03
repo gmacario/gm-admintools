95110dae807f8539edb06c0c6d64542b48ce5b1c
macario <macario@23512697-871b-0410-bc7e-d018c10ccd9e>
2009-10-03 08:08:52 +0000

    Support mirroring more than one smbproject
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@12543 23512697-871b-0410-bc7e-d018c10ccd9e

--- smb_mirror/smb_mirror.sh
+++ smb_mirror/smb_mirror.sh
@@ -49,88 +49,98 @@ REMOTE_MOUNTPOINT=${HOME}/remote
 MIRROR_BASEDIR=/var/www/mirrors
 
 # -----------------------------------------------------------------------------
-# Define NAS_SHARE, NAS_SOURCEDIR based on MIRROR_PROJECT
-
-if [ "${MIRROR_PROJECT}" = "" ]; then
-        read -p "Enter MIRROR_PROJECT: " MIRROR_PROJECT
-fi
-
-if [ "${MIRROR_PROJECT}" = "architects@multi" ]; then
-	NAS_SHARE=//multi.mmemea.marelliad.net/telematic_doc
-	NAS_SOURCEDIR="architects"
-fi
-
-if [ "${MIRROR_PROJECT}" = "SMEG_A9@multi" ]; then
-	NAS_SHARE=//multi.mmemea.marelliad.net/telematic_doc
-	NAS_SOURCEDIR="Projets/17_projetSMEG_A9"
-fi
-
-if [ "${MIRROR_PROJECT}" = "SMEG_OpenPlus_B78@multi" ]; then
-	NAS_SHARE=//multi.mmemea.marelliad.net/telematic_doc
-	NAS_SOURCEDIR="Projets/20_projet SMEG OPEN+ B78"
-fi
-
-# Use Venaria mirror for SMEG_A9@multi
-# (only for initial syncup, the folder is not kept updated...)
-if [ "${MIRROR_PROJECT}" = "SMEG@itven1nnas1" ]; then
-	NAS_SHARE=//itven1nnas1.venaria.marelli.it/smeg
-	NAS_SOURCEDIR=.
-fi
-
-# -----------------------------------------------------------------------------
 # You should not need to change the script below
 # -----------------------------------------------------------------------------
 
 # -----------------------------------------------------------------------------
+# Functions
+# -----------------------------------------------------------------------------
+
+# Mirror one project
+#
+# $1: Name of the project to be mirrored
+do_mirror_smbproject()
+{
+    MIRROR_PROJECT=$1
+
+    # Define NAS_SHARE, NAS_SOURCEDIR based on MIRROR_PROJECT
+    if [ "${MIRROR_PROJECT}" = "" ]; then
+        read -p "Enter MIRROR_PROJECT: " MIRROR_PROJECT
+    fi
+    if [ "${MIRROR_PROJECT}" = "architects@multi" ]; then
+	NAS_SHARE=//multi.mmemea.marelliad.net/telematic_doc
+	NAS_SOURCEDIR="architects"
+    fi
+    if [ "${MIRROR_PROJECT}" = "SMEG_A9@multi" ]; then
+	NAS_SHARE=//multi.mmemea.marelliad.net/telematic_doc
+	NAS_SOURCEDIR="Projets/17_projetSMEG_A9"
+    fi
+    if [ "${MIRROR_PROJECT}" = "SMEG_OpenPlus_B78@multi" ]; then
+	NAS_SHARE=//multi.mmemea.marelliad.net/telematic_doc
+	NAS_SOURCEDIR="Projets/20_projet SMEG OPEN+ B78"
+    fi
+    # Use Venaria mirror for SMEG_A9@multi
+    # (only for initial syncup, the folder is not kept updated...)
+    if [ "${MIRROR_PROJECT}" = "SMEG@itven1nnas1" ]; then
+	NAS_SHARE=//itven1nnas1.venaria.marelli.it/smeg
+	NAS_SOURCEDIR=.
+    fi
+
+    # Request parameters if still unknown
+    if [ "${NAS_SHARE}" = "" ]; then
+        read -p "Enter NAS_SHARE: " NAS_SHARE
+    fi
+    if [ "${NAS_SOURCEDIR}" = "" ]; then
+        read -p "Enter NAS_SOURCEDIR: " NAS_SOURCEDIR
+    fi
+
+    MIRROR_DESTDIR=${MIRROR_BASEDIR}/${MIRROR_PROJECT}
+    mkdir -p ${MIRROR_DESTDIR}
+
+    if [ "${OPT_MOUNT_SOURCEDIR}" = "true" ]; then
+    sudo mount -t cifs ${NAS_SHARE} ${REMOTE_MOUNTPOINT} \
+	-o user=${NAS_DOMAIN}/${NAS_USER} \
+	-o password=${NAS_PASSWORD} \
+	-o ro
+    fi
+
+    #rsync -avz ${REMOTE_MOUNTPOINT}/${NAS_SOURCEDIR}/ ${MIRROR_DESTDIR}
+    cmdline="rsync -avz \
+	${REMOTE_MOUNTPOINT}/${NAS_SOURCEDIR}/ \
+	${MIRROR_DESTDIR}"
+    echo "INFO: Executing ${cmdline}"
+    ${cmdline}
+
+    if [ "${OPT_MOUNT_SOURCEDIR}" = "true" ]; then
+	sudo umount ${REMOTE_MOUNTPOINT}
+    fi
+}
+
+# -----------------------------------------------------------------------------
 # Main Program starts here
-echo "INFO: $0 - v0.2"
+echo "INFO: $0 - v0.3"
 
 #set -x
 set -e
 
 ## Request parameters if not specified in the section above
-#BCK_FILENAME=`date +%Y%m%d`-${VM_NAME}
-if [ "${OPT_MOUNT_SOURCEDIR}" = "true" ]; then
-    if [ "${NAS_SHARE}" = "" ]; then
-        read -p "Enter NAS_SHARE: " NAS_SHARE
-    fi
-    if [ "${NAS_DOMAIN}" = "" ]; then
-        read -p "Enter NAS_DOMAIN: " NAS_DOMAIN
-    fi
-    if [ "${NAS_USER}" = "" ]; then
-        read -p "Enter NAS_USER: " NAS_USER
-    fi
-    if [ "${NAS_PASSWORD}" = "" ]; then
-        echo -n "Enter NAS_PASSWORD: "
-        stty -echo
-        read NAS_PASSWORD
-        echo
-        stty echo
-    fi
-    if [ "${NAS_SOURCEDIR}" = "" ]; then
-        read -p "Enter NAS_SOURCEDIR: " NAS_SOURCEDIR
-    fi
+if [ "${NAS_USER}" = "" ]; then
+    read -p "Enter NAS_USER: " NAS_USER
+fi
+if [ "${NAS_PASSWORD}" = "" ]; then
+    echo -n "Enter NAS_PASSWORD: "
+    stty -echo
+    read NAS_PASSWORD
+    echo
+    stty echo
 fi
 
-if [ ! -e ${REMOTE_MOUNTPOINT} ]; then
-	mkdir -p ${REMOTE_MOUNTPOINT}
-fi
+export NAS_USER
+export NAS_PASSWORD
 
-MIRROR_DESTDIR=${MIRROR_BASEDIR}/${MIRROR_PROJECT}
-mkdir -p ${MIRROR_DESTDIR}
-
-if [ "${OPT_MOUNT_SOURCEDIR}" = "true" ]; then
-sudo mount -t cifs ${NAS_SHARE} ${REMOTE_MOUNTPOINT} \
-	-o user=${NAS_DOMAIN}/${NAS_USER} \
-	-o password=${NAS_PASSWORD} \
-	-o ro
-fi
-
-set -x
-rsync -avz ${REMOTE_MOUNTPOINT}/${NAS_SOURCEDIR}/ ${MIRROR_DESTDIR}
-
-if [ "${OPT_MOUNT_SOURCEDIR}" = "true" ]; then
-	sudo umount ${REMOTE_MOUNTPOINT}
-fi
+do_mirror_smbproject "SMEG_A9@multi"
+# do_mirror_smbproject "architects@multi"
+# do_mirror_smbproject "SMEG_OpenPlus_B78"
+##do_mirror_smbproject "SMEG@itven1nnas1"
 
 # === EOF ===
