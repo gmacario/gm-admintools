da3e4ec8bce1fc58d301523239aab1c2a9809712
macario <macario@23512697-871b-0410-bc7e-d018c10ccd9e>
2010-02-24 04:53:40 +0000

    Implement dual command do_ssh_nopass_disable.sh
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@14004 23512697-871b-0410-bc7e-d018c10ccd9e

---
+++ ssh_utils/do_ssh_nopass_disable.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+# ============================================================================
+# Project:	LUPIN
+#
+# Description:	Disable SSH authentication without password request
+#
+# Language:	Unix Shell Script
+#
+# Required packages:
+#	openssh-client
+#
+# See also:
+#	LupinWiki:[[Secure_authentication_with_SSH_without_password_request]]
+#
+# Usage examples:
+#	do_ssh_nopass_disable.sh root@gianpinas.homelinux.net
+# ============================================================================
+
+# ---------------------------------------------------------------------------
+# Configurable parameters
+# ---------------------------------------------------------------------------
+
+# NONE
+
+# ---------------------------------------------------------------------------
+# Main Program
+# ---------------------------------------------------------------------------
+
+#set -x
+set -e
+
+PROGNAME=`basename $0`
+echo "INFO: ${PROGNAME} - v0.1"
+
+if [ $# -lt 1 ]; then
+    echo "Usage: ${PROGNAME} remoteuser@remotehost"
+    exit 1
+fi
+
+REMOTEUSER=`echo $1 | sed -e 's/\@.*$//'`
+REMOTEHOST=`echo $1 | sed -e 's/^.*@//'`
+
+#echo "DEBUG: REMOTEUSER=$REMOTEUSER"
+#echo "DEBUG: REMOTEHOST=$REMOTEHOST"
+
+echo "INFO: Disabling automatic login to ${REMOTEUSER}@${REMOTEHOST}"
+
+if [ ! -e ${HOME}/.ssh/id_rsa.pub ]; then
+    echo "WARNING: Missing SSH public/private keypair"
+    ssh-keygen
+fi
+
+cat ${HOME}/.ssh/id_rsa.pub | ssh ${REMOTEUSER}@${REMOTEHOST} \
+"cd .ssh && \
+cp authorized_keys authorized_keys.OLD && \
+cat >disablekey.tmp && \
+grep -v -f disablekey.tmp authorized_keys.OLD >authorized_keys && \
+chmod 640 authorized_keys"
+
+# === EOF ===
