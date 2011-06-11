82176634ba761278b1b87928d5b2e3d39639aa27
macario <macario@23512697-871b-0410-bc7e-d018c10ccd9e>
2011-06-11 21:26:47 +0000

    Implement remote backup in addition to local one
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@23548 23512697-871b-0410-bc7e-d018c10ccd9e

--- gpg_utils/do_backup_gnupg.sh
+++ gpg_utils/do_backup_gnupg.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
 
+# Configurable parameters
+REMOTEUSER="gmacario"
+REMOTEHOST="itven1d0400.venaria.marelli.it"
+
 BACKUPDIR="${HOME}/BACKUP"
 #BACKUPDIR="/opt/BACKUP"
 
 TODAY="`date '+%Y%m%d'`"
-HOSTNAME="`hostname`"
-BK_FILE="${TODAY}-${HOSTNAME}-${USER}-gnupg.zip"
 
-cd
-zip -urp "${BACKUPDIR}/${BK_FILE}" .gnupg
+#set -x
+
+if [ "${REMOTEUSER}" != "" ]; then
+    BK_FILE="${TODAY}-${REMOTEHOST}-${REMOTEUSER}-gnupg.zip"
+
+    echo "INF: Backing up GPG keys of ${REMOTEUSER}@${REMOTEHOST}"
+    ssh "${REMOTEUSER}@${REMOTEHOST}" \
+	"zip -rp - .gnupg" > "${BACKUPDIR}/${BK_FILE}"
+else
+    HOSTNAME="`hostname`"
+    BK_FILE="${TODAY}-${HOSTNAME}-${USER}-gnupg.zip"
+
+    echo "INF: Backing up GPG keys of ${USER}@${HOSTNAME}"
+    cd
+    zip -rp "${BACKUPDIR}/${BK_FILE}" .gnupg
+fi
+
+echo "INF: GPG keys backed up at ${BACKUPDIR}/${BK_FILE}"
 
 # === EOF ===
