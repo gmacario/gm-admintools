e4394c70babbe75c38a281e3bee7b28772b7979d
macario <macario@23512697-871b-0410-bc7e-d018c10ccd9e>
2011-06-11 05:33:27 +0000

    Create sample script to restore SVN repos
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@23527 23512697-871b-0410-bc7e-d018c10ccd9e

--- svn_utils/do_backup_svnrepos.sh
+++ svn_utils/do_backup_svnrepos.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 # =============================================================================
 # Backup all SVN Repositories available on a Remote Host
 # =============================================================================
@@ -33,7 +34,8 @@ BACKUPDIR="${BK_BASEDIR}`date '+%Y%m%d'`-${REMOTEHOST}"
 #	svnadmin: Can't read length line in file '/opt/repos/mmseti/db/revs/6'
 #
 
-echo "INFO: $0 - v0.3"
+PROGNAME="`basename $0`"
+echo "INFO: ${PROGNAME} - v0.3"
 
 mkdir -p $BACKUPDIR || exit 1
 cd $BACKUPDIR || exit 1
@@ -44,14 +46,39 @@ scp "${REMOTEUSER}@${REMOTEHOST}:/etc/apache2/dav_svn.authz" .
 
 for repos in ${REPOSITORIES}; do
     echo "INFO: Dumping SVN repos $repos from $REMOTEHOST..."
-    (ssh ${REMOTEUSER}@${REMOTEHOST} svnadmin dump /opt/svnrepos/$repos | gzip -c -9) \
-	| split -b 2048m -d - ${NOW}-bk-$repos.svndump.gz-split
+    (ssh ${REMOTEUSER}@${REMOTEHOST} \
+	svnadmin dump /opt/svnrepos/${repos} \
+	| gzip -c -9) \
+	| split -b 2048m -d - ${NOW}-bk-${repos}.svndump.gz-split
     retval=$?
     if [ $retval -ne 0 ]; then
-	echo "ERROR: Dumping repository $repos returned $retval";
+	echo "ERROR: Dumping repository $repos returned ${retval}";
 	exit 1
     fi
+
+    echo "INFO: Creating sample script to restore SVN repos"
+    samplescript="sample-restore-${repos}.sh"
+    (
+	echo "#!/bin/sh"
+	echo "# Sample script to restore ${repos}"
+	echo "# http://svnbook.red-bean.com/en/1.5/svn.reposadmin.maint.html"
+	echo ""
+	echo "NEWREPOS=new-${repos}"
+	echo "FILES=${NOW}-bk-${repos}.svndump.gz-split*"
+	echo ""
+	echo "#set -x"
+	echo ""
+	echo "#zcat \${FILES} | hexdump -Cv"
+	echo "#zcat \${FILES} > dumpfile"
+	echo ""
+	echo "svnadmin create \${NEWREPOS}"
+	echo "zcat \${FILES} | svnadmin load \${NEWREPOS}"
+	echo ""
+	echo "# === EOF ==="
+    ) >"${samplescript}"
+    chmod 755 "${samplescript}"
 done
-echo "INFO: Dumping repositories to $BACKUPDIR completed"
+
+echo "INFO: ${PROGNAME} completed"
 
 # === EOF ===
