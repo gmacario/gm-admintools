3b93951aa4d070f0213a335331f76ac29d28cc5d
macario <macario@23512697-871b-0410-bc7e-d018c10ccd9e>
2011-06-11 06:42:34 +0000

    First version of script to backup GnuPG data
    git-svn-id: http://lupin05.venaria.marelli.it/svnrepos/lupin/platforms/LUPIN/code/trunk/misc/admin_scripts@23528 23512697-871b-0410-bc7e-d018c10ccd9e

---
+++ gpg_utils/do_backup_gnupg.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+BACKUPDIR="/opt/BACKUP"
+
+TODAY="`date '+%Y%m%d'`"
+HOSTNAME="`hostname`"
+BK_FILE="${TODAY}-${HOSTNAME}-${USER}-gnupg.zip"
+
+cd
+zip -urp "${BACKUPDIR}/${BK_FILE}" .gnupg
+
+# === EOF ===
